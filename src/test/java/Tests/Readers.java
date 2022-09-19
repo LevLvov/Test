341cacb4418f07e724dbbaa7932309b70fe43efb
LvovLev <metall_l@ukr.net>
2022-09-19 17:08:34 +0300

    ch

--- src/test/java/Tests/Readers.java
+++ src/test/java/Tests/Readers.java
@@ -1,2 +1,35 @@
-package Tests;public class Readers {
-}
+package Tests;
+
+public class Readers {
+    private String PIP;
+    private int card;
+    private int tel;
+    private int tookbooks;
+    private int takingbooks;
+    private int returnbooks;
+
+    public Readers(String PIP, int card, int tel) {
+        this.PIP = PIP;
+        this.card = card;
+        this.tel = tel;
+    }
+
+    public void takeBooks(int tookbooks, int takingbooks) {
+        if (tookbooks > 5) {
+            int shtraf = (tookbooks - 5) * 50;
+            System.out.println("Limit perevisheno. Suma shtrafy " + shtraf + "grn");}
+            System.out.println("Chitach " + PIP + " vzyav syogodny " + takingbooks);
+        }
+
+    public void returnBooks(int tookbooks, int returnbooks) {
+        int zalishok = tookbooks - returnbooks;
+        int shtraf = zalishok * 50;
+        int shtraf2 = (tookbooks-5)*50;
+        if (tookbooks > 5) {
+            System.out.println(PIP + " narahovano " + shtraf2 + "grn");
+        }
+        if (zalishok > 5) {
+            System.out.println("Takozh " + PIP + " narahovano dodatkovo " + shtraf + "grn");}
+            System.out.println(PIP + " orel");
+        }
+    }
--- src/test/java/Tests/ReadersData.java
+++ src/test/java/Tests/ReadersData.java
@@ -1,2 +1,13 @@
-package Tests;public class ReadersData {
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class ReadersData {
+    @Test
+    public void readersData() {
+        Readers Loh = new Readers("Lvov Lev", 1, 45);
+        Loh.takeBooks(6, 6);
+
+    Loh.returnBooks(2,2);
+    }
 }
